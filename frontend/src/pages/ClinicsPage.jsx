df6eaf8aafe63ded6064bd538261abdec3fd4589
taqiuddincb586 <taqiuddin.cb586@gmail.com>
2026-03-01 12:46:15 +0300

    Update ClinicsPage.jsx

--- frontend/src/pages/ClinicsPage.jsx
+++ frontend/src/pages/ClinicsPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import api from '../services/api'
 import toast from 'react-hot-toast'
